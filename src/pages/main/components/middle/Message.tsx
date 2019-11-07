fec0aed36c051aa550dce42ce4d429af85c3bf92
Alexander Zinchuk <alx.zinchuk@gmail.com>
2019-11-07 20:00:01 +0300

    Support emoji-only messages

--- src/pages/main/components/middle/Message.tsx
+++ src/pages/main/components/middle/Message.tsx
@@ -1,12 +1,10 @@
 import React, { FC } from '../../../../lib/teact';
 import { DispatchMap, withGlobal } from '../../../../lib/teactn';
-
 import { ApiMessage } from '../../../../modules/tdlib/types/messages';
 
 import { getMessageText, isOwnMessage } from '../../../../modules/tdlib/helpers';
-
+import parseEmojiOnlyString from '../../../../util/parseEmojiOnlyString';
 import Avatar from '../../../../components/Avatar';
-
 import './Message.scss';
 
 type IProps = {
@@ -15,11 +13,20 @@ type IProps = {
 } & Pick<DispatchMap, 'selectMessage'>;
 
 const Message: FC<IProps> = ({ message, isSelected }) => {
+  const text = getMessageText(message);
+
+  let className;
+  if (text) {
+    const emojiOnlyCount = parseEmojiOnlyString(text);
+    const emojiOnlyClassName = emojiOnlyCount ? `emoji-only-${emojiOnlyCount}` : null;
+
+    className = emojiOnlyClassName || 'text';
+  }
 
   return (
     <div className={`Message ${isSelected ? 'selected' : ''} ${isOwnMessage(message) ? 'own' : ''}`}>
       <Avatar size="small">HE</Avatar>
-      <div className="text">{getMessageText(message)}</div>
+      <div className={className}>{getMessageText(message)}</div>
     </div>
   );
 };
---
+++ src/util/parseEmojiOnlyString.ts
@@ -0,0 +1,20 @@
+// @ts-ignore
+import emojiRegex from 'emoji-regex';
+
+const RE_EMOJI = emojiRegex();
+const RE_EMOJI_ONLY = new RegExp('^(?:' + emojiRegex().source + ')+$', '');
+
+export default (text: string): number | false => {
+  const isEmojiOnly = RE_EMOJI_ONLY.exec(text);
+
+  if (!isEmojiOnly) {
+    return false;
+  }
+
+  let emojiCount = 0;
+  while(RE_EMOJI.exec(text)) {
+    emojiCount++
+  }
+
+  return emojiCount;
+}
