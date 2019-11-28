3f6c3b528265e21a9f26ba134804dd6e2a560dd6
Sato <sato.rc@yandex.ru>
2019-11-28 14:39:46 +0300

    Add support for multiline text (#72)
    * Extract message input into a component, support multiline text
    
    * Add max-height to message input, support scrolling
    
    * Add comment regarding `onNextTick`

---
+++ src/pages/main/components/middle/MessageInput.tsx
@@ -0,0 +1,86 @@
+import { ChangeEvent, KeyboardEvent } from 'react';
+import React, { FC, useState } from '../../../../lib/teact';
+import { withGlobal } from '../../../../lib/teactn';
+
+import { GlobalActions } from '../../../../store/types';
+import { onNextTick } from '../../../../util/schedulers';
+import './MiddleFooter.scss';
+
+type IProps = Pick<GlobalActions, 'sendTextMessage'> & {
+  selectedChatId: number;
+};
+
+const MAX_INPUT_HEIGHT = 240;
+
+const MiddleFooter: FC<IProps> = ({ selectedChatId, sendTextMessage }) => {
+  const [messageText, setMessageText] = useState('');
+
+  function onChange(e: ChangeEvent<HTMLTextAreaElement>) {
+    const { currentTarget } = e;
+    const value = currentTarget.value.trim();
+    setMessageText(value);
+    if (currentTarget.scrollHeight !== currentTarget.offsetHeight) {
+      currentTarget.style.height = 'auto';
+      currentTarget.style.height = `${Math.min(currentTarget.scrollHeight, MAX_INPUT_HEIGHT)}px`;
+    }
+
+    currentTarget.style.overflowY = currentTarget.scrollHeight <= MAX_INPUT_HEIGHT ? 'hidden' : 'auto';
+  }
+
+  function onKeyPress(e: KeyboardEvent<HTMLTextAreaElement>) {
+    const { currentTarget } = e;
+    const value = currentTarget.value.trim();
+
+    if (e.keyCode === 13 && !e.shiftKey && value.length) {
+      sendTextMessage({
+        chatId: selectedChatId,
+        text: value,
+      });
+
+      // Make sure to clear the text after the latest `onChange`.
+      // Using setTimeout as `onNextTick` somehow doesn't work here.
+      setTimeout(() => {
+        setMessageText('');
+        currentTarget.removeAttribute('style');
+      }, 0);
+    }
+  }
+
+  onNextTick(focusInput);
+
+  return (
+    <textarea
+      id="message-input-text"
+      className="form-control custom-scroll"
+      placeholder="Message"
+      rows={1}
+      autoComplete="off"
+      onChange={onChange}
+      onKeyPress={onKeyPress}
+      value={messageText}
+    />
+  );
+};
+
+function focusInput() {
+  const input = document.getElementById('message-input-text');
+
+  if (input) {
+    input.focus();
+  }
+}
+
+
+export default withGlobal(
+  (global) => {
+    const { chats } = global;
+
+    return {
+      selectedChatId: chats.selectedId,
+    };
+  },
+  (setGlobal, actions) => {
+    const { sendTextMessage } = actions;
+    return { sendTextMessage };
+  },
+)(MiddleFooter);
--- src/pages/main/components/middle/MiddleFooter.tsx
+++ src/pages/main/components/middle/MiddleFooter.tsx
@@ -1,55 +1,19 @@
-import { ChangeEvent, KeyboardEvent } from 'react';
-import React, { FC, useState } from '../../../../lib/teact';
-import { withGlobal } from '../../../../lib/teactn';
+import React, { FC } from '../../../../lib/teact';
 
-import { GlobalActions } from '../../../../store/types';
 import Button from '../../../../components/ui/Button';
-import { onNextTick } from '../../../../util/schedulers';
-import InputText from '../../../../components/ui/InputText';
+import MessageInput from './MessageInput';
 import './MiddleFooter.scss';
 
-type IProps = Pick<GlobalActions, 'sendTextMessage'> & {
-  selectedChatId: number;
-};
+type IProps = {};
 
-const MiddleFooter: FC<IProps> = ({ selectedChatId, sendTextMessage }) => {
-  const [messageText, setMessageText] = useState('');
-
-  function onChange(e: ChangeEvent<HTMLInputElement>) {
-    const { currentTarget } = e;
-    setMessageText(currentTarget.value.trim());
-  }
-
-  function onKeyPress(e: KeyboardEvent<HTMLInputElement>) {
-    const { currentTarget } = e;
-
-    if (e.keyCode === 13 && currentTarget.value.trim().length) {
-      sendTextMessage({
-        chatId: selectedChatId,
-        text: currentTarget.value,
-      });
-
-      // Make sure to clear the text after the latest `onChange`.
-      setTimeout(() => setMessageText(''), 0);
-    }
-  }
-
-  onNextTick(focusInput);
-
+const MiddleFooter: FC<IProps> = () => {
   return (
     <div className="MiddleFooter">
       <div id="message-input-wrapper">
         <Button className="not-implemented" round color="translucent">
           <i className="icon-smile" />
         </Button>
-        {/* TODO Convert to textarea, add auto-sizing */}
-        <InputText
-          id="message-input-text"
-          placeholder="Message"
-          onChange={onChange}
-          onKeyPress={onKeyPress}
-          value={messageText}
-        />
+        <MessageInput />
         <Button className="not-implemented" round color="translucent">
           <i className="icon-attach" />
         </Button>
@@ -61,24 +25,4 @@ const MiddleFooter: FC<IProps> = ({ selectedChatId, sendTextMessage }) => {
   );
 };
 
-function focusInput() {
-  const input = document.getElementById('message-input-text');
-
-  if (input) {
-    input.focus();
-  }
-}
-
-export default withGlobal(
-  (global) => {
-    const { chats } = global;
-
-    return {
-      selectedChatId: chats.selectedId,
-    };
-  },
-  (setGlobal, actions) => {
-    const { sendTextMessage } = actions;
-    return { sendTextMessage };
-  },
-)(MiddleFooter);
+export default MiddleFooter;
