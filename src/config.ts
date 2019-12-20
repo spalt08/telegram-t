891113a7e506ac0d52c6bdc3a0612645f4e03292
Alexander Zinchuk <alx.zinchuk@gmail.com>
2019-12-20 15:15:53 -0500

    [TL] Add missing entities

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const DEBUG = true;
 
-export const MEDIA_CACHE_DISABLED = true;
+export const MEDIA_CACHE_DISABLED = false;
 export const MEDIA_CACHE_NAME = 'tt-media';
 
-export const GLOBAL_STATE_CACHE_DISABLED = true;
+export const GLOBAL_STATE_CACHE_DISABLED = false;
 export const GLOBAL_STATE_CACHE_KEY = 'tt-global-state';
 
 export const GRAMJS_SESSION_ID_KEY = 'GramJs:sessionId';
