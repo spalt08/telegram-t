a25bbfe691fedbfb56f310e22e6476c224350962
Alexander Zinchuk <alx.zinchuk@gmail.com>
2020-02-10 18:23:47 +0100

    Fix and improve `useShowTransition`

--- src/hooks/useShowTransition.ts
+++ src/hooks/useShowTransition.ts
@@ -3,38 +3,35 @@ import { useEffect, useRef, useState } from '../lib/teact/teact';
 const CLOSE_DURATION = 350;
 
 export default (isOpen = false, onCloseTransitionEnd?: () => void, noOpenTransition = false) => {
-  const [shouldRender, setShouldRender] = useState(isOpen);
+  const [isClosed, setIsClosed] = useState(!isOpen);
   const closeTimeoutRef = useRef<number>();
   // СSS class should be added in a separate tick to turn on CSS transition.
   const [hasAsyncOpenClassName, setHasAsyncOpenClassName] = useState(false);
 
   useEffect(() => {
-    setHasAsyncOpenClassName(isOpen && shouldRender);
+    setHasAsyncOpenClassName(isOpen);
+  }, [isOpen]);
 
-    if (isOpen) {
-      setShouldRender(true);
+  if (isOpen) {
+    setIsClosed(false);
 
-      if (closeTimeoutRef.current) {
-        window.clearTimeout(closeTimeoutRef.current);
-        closeTimeoutRef.current = null;
-      }
-    } else if (!closeTimeoutRef.current) {
-      closeTimeoutRef.current = window.setTimeout(() => {
-        if (isOpen) {
-          return;
-        }
-
-        setShouldRender(false);
-
-        if (onCloseTransitionEnd) {
-          onCloseTransitionEnd();
-        }
-
-        closeTimeoutRef.current = null;
-      }, CLOSE_DURATION);
+    if (closeTimeoutRef.current) {
+      window.clearTimeout(closeTimeoutRef.current);
+      closeTimeoutRef.current = null;
     }
-  }, [isOpen, shouldRender, onCloseTransitionEnd]);
+  } else if (!isClosed && !closeTimeoutRef.current) {
+    closeTimeoutRef.current = window.setTimeout(() => {
+      setIsClosed(true);
 
+      if (onCloseTransitionEnd) {
+        onCloseTransitionEnd();
+      }
+
+      closeTimeoutRef.current = null;
+    }, CLOSE_DURATION);
+  }
+
+  const shouldRender = isOpen || closeTimeoutRef.current;
   const transitionClassNames = [];
   const hasOpenClassName = hasAsyncOpenClassName || (isOpen && noOpenTransition);
   if (hasOpenClassName) {
