269d547f36c5a203a7b2e70ae1bd83fa9cc480bc
Alexander Zinchuk <alx.zinchuk@gmail.com>
2020-02-27 15:24:25 +0100

    Spinner: Remove unused code

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -4,17 +4,14 @@ import './Spinner.scss';
 
 const Spinner: FC<{
   color?: 'blue' | 'white' | 'black';
-  progress?: number;
   background?: boolean;
 }> = ({
   color = 'blue',
-  progress,
   background,
 }) => {
   return (
     <div className={`Spinner ${color}${background ? ' with-background' : ''}`}>
       <div />
-      {typeof progress === 'number' && <span>{Math.round(progress * 100)}%</span>}
     </div>
   );
 };
