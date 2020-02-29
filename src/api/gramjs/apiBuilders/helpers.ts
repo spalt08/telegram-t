7f1541d3fdf258cd865eae157b3dfb84b4e49011
Alexander Zinchuk <alx.zinchuk@gmail.com>
2020-02-29 14:11:18 +0100

    API: Optimize `bytesToDataUri`

--- src/api/gramjs/apiBuilders/helpers.ts
+++ src/api/gramjs/apiBuilders/helpers.ts
@@ -1,20 +1,5 @@
-import { Api as GramJs } from '../../../lib/gramjs';
-
 export function bytesToDataUri(bytes: Buffer, shouldOmitPrefix = false, mimeType: string = 'image/jpg') {
   const prefix = shouldOmitPrefix ? '' : `data:${mimeType};base64,`;
 
-  return `${prefix}${btoa(
-    bytes.reduce((data, byte) => {
-      return data + String.fromCharCode(byte);
-    }, ''),
-  )}`;
-}
-
-export function omitGramJsFields<T extends GramJs.VirtualClass<{}>>(
-  instance: T,
-): Omit<T, 'CONSTRUCTOR_ID' | 'SUBCLASS_OF_ID'> {
-  const object = { ...instance };
-  delete object.CONSTRUCTOR_ID;
-  delete object.SUBCLASS_OF_ID;
-  return object;
+  return `${prefix}${btoa(String.fromCharCode(...bytes))}`;
 }
