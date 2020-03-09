dd36cf5b6e13fdd08cce4b3ff2769d99cc8cf023
Sato <sato.rc@yandex.ru>
2020-03-09 21:58:29 +0300

    Message: Allow clicking on sender name and mentions (#349)

--- src/modules/selectors/users.ts
+++ src/modules/selectors/users.ts
@@ -4,6 +4,12 @@ export function selectUser(global: GlobalState, userId: number) {
   return global.users.byId[userId];
 }
 
+export function selectUserByUserName(global: GlobalState, userName: string) {
+  return Object.values(global.users.byId).find(
+    (user) => user.username.toLowerCase() === userName.toLowerCase(),
+  );
+}
+
 export function selectOpenUser(global: GlobalState) {
   const { byId, selectedId } = global.users;
   return selectedId ? byId[selectedId] : undefined;
