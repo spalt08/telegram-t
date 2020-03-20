14db255af42bdbf7b2769af682169e986f3e11c2
Alexander Zinchuk <alx.zinchuk@gmail.com>
2020-03-20 22:23:34 +0100

    Profile: Fix opening user info in private chat

--- src/modules/actions/users.ts
+++ src/modules/actions/users.ts
@@ -3,9 +3,16 @@ import { addReducer } from '../../lib/teact/teactn';
 import { GlobalState } from '../../global/types';
 
 import { updateSelectedUserId } from '../reducers';
+import { selectOpenChat } from '../selectors';
 
 addReducer('openUserInfo', (global, actions, payload) => {
   const { id } = payload!;
+  const selectedChat = selectOpenChat(global);
+
+  if (selectedChat && selectedChat.id === id) {
+    actions.openChatWithInfo({ id });
+    return undefined;
+  }
 
   return updateSelectedUserId(global, id);
 });
