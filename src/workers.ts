519386db29d43a4b8c8388104b4e117811cc00c4
Alexander Zinchuk <alx.zinchuk@gmail.com>
2020-04-29 21:50:37 +0200

    Fix #407 Sign Up: Unable to upload profile picture

--- src/workers.ts
+++ src/workers.ts
@@ -1,4 +1,4 @@
-// For some reason workers are not properly bundled when imported from async modules,
+// @parcel For some reason workers are not properly bundled when imported from async modules,
 // so we need to also import them from the main bundle. Probably will be fixed after upgrading Parcel.
 
 export function initOpusWorker() {
