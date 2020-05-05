0fab699d019307519b7b71c969e5150c3269182d
Alexander Zinchuk <alx.zinchuk@gmail.com>
2020-05-05 20:04:39 +0200

    Resolve #474 Composer: Preprocess large JPGs and PNGs when attaching as quick photo

--- src/components/middle/composer/helpers/buildAttachment.ts
+++ src/components/middle/composer/helpers/buildAttachment.ts
@@ -2,6 +2,7 @@ import { ApiAttachment } from '../../../../api/types';
 import { preloadImage, preloadVideo, createPosterForVideo } from '../../../../util/files';
 
 const MAX_QUICK_VIDEO_SIZE = 10 * 1024 ** 2; // 10 MB
+const MAX_QUICK_IMG_SIZE = 1280; // px
 
 export default async function buildAttachment(
   filename: string, blob: Blob, isQuick: boolean, options?: Partial<ApiAttachment>,
@@ -11,19 +12,26 @@ export default async function buildAttachment(
   let quick;
   let previewBlobUrl;
 
-  // Videos under 10 MB display as regular videos in other clients regardless of chosen attachment option
-  if (
-    isQuick
-    || (mimeType.startsWith('video/') && size < MAX_QUICK_VIDEO_SIZE)
-  ) {
-    if (mimeType.startsWith('image/')) {
-      const { width, height } = await preloadImage(blobUrl);
-      quick = { width, height };
-    } else {
-      const { videoWidth: width, videoHeight: height, duration } = await preloadVideo(blobUrl);
-      quick = { width, height, duration };
-      previewBlobUrl = await createPosterForVideo(blobUrl);
+  if (isQuick && mimeType.startsWith('image/')) {
+    const img = await preloadImage(blobUrl);
+    const { width, height } = img;
+
+    if (width > MAX_QUICK_IMG_SIZE || height > MAX_QUICK_IMG_SIZE || mimeType !== 'image/jpeg') {
+      const newBlob = await squeezeImage(img);
+      if (newBlob) {
+        URL.revokeObjectURL(blobUrl);
+        return buildAttachment(filename, newBlob, true, options);
+      } else {
+        return buildAttachment(filename, blob, false, options);
+      }
     }
+
+    quick = { width, height };
+    // Videos under 10 MB display as regular videos in other clients regardless of chosen attachment option
+  } else if (isQuick && mimeType.startsWith('video/') && size < MAX_QUICK_VIDEO_SIZE) {
+    const { videoWidth: width, videoHeight: height, duration } = await preloadVideo(blobUrl);
+    quick = { width, height, duration };
+    previewBlobUrl = await createPosterForVideo(blobUrl);
   } else if (mimeType.startsWith('image/')) {
     previewBlobUrl = blobUrl;
   } else if (mimeType.startsWith('video/')) {
@@ -39,4 +47,29 @@ export default async function buildAttachment(
     previewBlobUrl,
     ...options,
   };
+}
+
+function squeezeImage(img: HTMLImageElement): Promise<Blob | null> {
+  return new Promise((resolve) => {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d')!;
+
+    let { width, height } = img;
+
+    if (width > MAX_QUICK_IMG_SIZE || height > MAX_QUICK_IMG_SIZE) {
+      if (width >= height) {
+        height *= MAX_QUICK_IMG_SIZE / width;
+        width = MAX_QUICK_IMG_SIZE;
+      } else {
+        width *= MAX_QUICK_IMG_SIZE / height;
+        height = MAX_QUICK_IMG_SIZE;
+      }
+    }
+
+    canvas.width = width;
+    canvas.height = height;
+
+    ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, width, height);
+    canvas.toBlob(resolve, 'image/jpeg', 100);
+  });
 }
